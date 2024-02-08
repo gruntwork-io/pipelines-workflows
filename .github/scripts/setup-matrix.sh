5dd7160d9ec0164746f693ebfa8b4043236b24b6
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2024-02-08 18:15:33 -0500

    Removing references to `infra_live_directory`

---
+++ .github/scripts/setup-matrix.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+# This script is used to prepare the relevant outputs required to run the 2nd and 3rd jobs of the
+# apply-new-sdlc-accounts-baseline.yml workflow.
+
+# The most important output it creates
+# is the `env_ids`, which needs to be determined here for the next job to start with a matrix
+# of all the environment ids. This has to be done before the next job starts, as GitHub workflows require
+# a static string to be used to define the elements of a matrix. By creating a JSON string representing the
+# array of environments to be used in the matrix, we can then use the `fromJSON` function in the next job
+# to convert the string into an array of environment ids.
+
+# The rest of the outputs are just convenience outputs to avoid having to repeat the same logic in each
+# iteration of the matrix.
+
+set -euo pipefail
+IFS=$'\n\t'
+
+# Required environment variables
+
+# A JSON string representing the team account data for the team account that is being created.
+: "${TEAM_ACCOUNT_DATA:? TEAM_ACCOUNT_DATA is a required environment variable}"
+
+# The path to the infra-live repo. The infra-live repo is inspected to determine the list of environments
+# that need to be created, along with some other metadata about the account creation request.
+: "${PATH_TO_INFRA_LIVE:? PATH_TO_INFRA_LIVE is a required environment variable}"
+
+get_env_ids() {
+    readonly path_to_infra_live="$1"
+    readonly gruntwork_config_path="${path_to_infra_live}/.gruntwork/config.yml"
+    readonly account_type="$2"
+
+    local path=''
+
+    case "$account_type" in
+    'sdlc')
+        path='.pipelines.account-vending.sdlc.account-identifiers'
+        ;;
+    'sandbox')
+        path='.pipelines.account-vending.sandbox.account-identifiers'
+        ;;
+    *)
+        echo "Account type '$account_type' is not recognised. Exiting."
+        exit 1
+        ;;
+    esac
+
+    yq --no-colors -o=json -I=0 "$path" "$gruntwork_config_path"
+}
+
+first_account_key="$(jq '. | keys[] | select(. | endswith("AccountName"))' <<< "$TEAM_ACCOUNT_DATA" | jq -s -r '.[0]')"
+first_account_name="$(jq -r --arg first_account_key "$first_account_key" '.[$first_account_key]' <<< "$TEAM_ACCOUNT_DATA")"
+
+request_file_path="${PATH_TO_INFRA_LIVE}/_new-account-requests/account-${first_account_name}.yml"
+requesting_team_name="$(yq -r '.requesting_team_name' "$request_file_path")"
+requesting_team_id="$(yq -r '.requesting_team_id' "$request_file_path")"
+account_type="$(yq -r '.account_type' "$request_file_path")"
+
+org_name_prefix="$(yq -r '.org_name_prefix' "$request_file_path")"
+aws_region="$(yq -r '.aws_region' "$request_file_path")"
+tags="$(yq -o=json -I=0 '.tags' "$request_file_path")"
+
+full_team_name="${requesting_team_name}-${requesting_team_id}"
+new_infra_live_repo_name="infra-live-${full_team_name}"
+
+env_ids="$(get_env_ids "$PATH_TO_INFRA_LIVE" "$account_type")"
+
+if [[ $env_ids == 'null' || "$(jq '. | length' <<< "$env_ids")" == '0' ]]; then
+    echo "No environments found in the infra-live repo. Exiting."
+    exit 1
+fi
+
+create_vpc="$(yq -r '.create_vpc' "$request_file_path")"
+
+# This handles the edge case where the create_vpc field is not set in the account creation request.
+# If so, we'll default to creating a VPC.
+if [[ $create_vpc == 'null' ]]; then
+    create_vpc='true'
+fi
+
+# Outputs required in subsequent steps in the workflow in order create the new team's infrastructure-live repo
+{
+    echo "full_team_name=$full_team_name"
+    echo "new_infra_live_repo_name=$new_infra_live_repo_name"
+    echo "org_name_prefix=$org_name_prefix"
+    echo "aws_region=$aws_region"
+    echo "tags=$tags"
+    echo "env_ids=$env_ids"
+    echo "create_vpc=$create_vpc"
+} >>"$GITHUB_OUTPUT"
