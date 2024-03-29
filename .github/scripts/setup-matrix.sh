e15a5934618e94072c431ca84c89069afb5be8b9
Zach Goldberg <zach@zachgoldberg.com>
2024-03-29 16:54:03 -0700

    include delegated account workflow (unused for now)

--- .github/scripts/setup-matrix.sh
+++ .github/scripts/setup-matrix.sh
@@ -78,8 +78,35 @@ if [[ $create_vpc == 'null' ]]; then
     create_vpc='true'
 fi
 
+delegate_management="$(yq -r '.delegate_management // false' "$request_file_path")"
+readonly delegate_management
+delegate_repo_name="$(yq -r '.delegate_repo_name // ""' "$request_file_path")"
+readonly delegate_repo_name
+
+get_catalog_repositories() {
+    local -r path_to_infra_live="$1"
+    local -r gruntwork_config_path="${path_to_infra_live}/.gruntwork/config.yml"
+    local -r account_type="$2"
+
+    local -r path=".pipelines.account-vending.${account_type}.catalog-repositories"
+
+    if [[ "$(yq "$path" "$gruntwork_config_path")" == null ]]; then
+        echo "[]"
+    else
+        yq --no-colors -o=json -I=0 "$path" "$gruntwork_config_path"
+    fi
+}
+
+account_type="$(yq -r '.account_type' "$request_file_path")"
+readonly account_type
+catalog_repositories="$(get_catalog_repositories "$PATH_TO_INFRA_LIVE" "$account_type")"
+readonly catalog_repositories
+
 # Outputs required in subsequent steps in the workflow in order create the new team's infrastructure-live repo
 {
+    echo "delegate_management=$delegate_management"
+    echo "delegate_repo_name=$delegate_repo_name"
+    echo "catalog_repositories=$catalog_repositories"
     echo "full_team_name=$full_team_name"
     echo "new_infra_live_repo_name=$new_infra_live_repo_name"
     echo "org_name_prefix=$org_name_prefix"
